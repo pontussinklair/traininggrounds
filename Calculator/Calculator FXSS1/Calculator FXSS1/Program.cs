eb9197d51c5eb5d177d025fe640d9888afb41f03
Pontus Eriksson <pontus.sinklair@gmail.com>
2021-03-05 11:14:03 +0100

    Finished

--- Calculator/Calculator FXSS1/Calculator FXSS1/Program.cs
+++ Calculator/Calculator FXSS1/Calculator FXSS1/Program.cs
@@ -8,29 +8,36 @@ namespace Calculator_FXSS1
         {
             return num1 + num2;
         }
-        // exempel Addition([10,20,30])
 
         public static double Addition(double[] numbers)
         {
             double sum = 0;
-            for (int i = 0; i < numbers.Length; i++)
+            foreach (double number in numbers)
             {
-                sum = sum + numbers[i];
+                sum = number + sum;
+            }
+            return sum;
+
+        }
+        public static double Subtraction(double[] numbers)
+        {
+            double sum = 0;
+            foreach (double number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
 
             }
-
-            Console.WriteLine(sum);
-
-            return sum;
-        }
         public static double Subtraction(double num1, double num2)
         {
             return num1 - num2;
         }
-        public static double Multiplication(double num1, double num2)
+            public static double Multiplication(double num1, double num2)
         {
             return num1 * num2;
         }
+        
         public static double Division(double num1, double num2)
         {
             if (num2 == 0)
@@ -40,11 +47,9 @@ namespace Calculator_FXSS1
                 Console.ResetColor();
             }
             return num1 / num2;  
-            
         }
         public static void Main(string[] args)
         {
-
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Hi and welcome to the FXSS1 Mathematical Simulator!");
             Console.WriteLine(); // Empty line.
@@ -53,8 +58,6 @@ namespace Calculator_FXSS1
             Console.ReadKey(true);    // Wait for input key to continue
 
             bool keepAlive = true;
-
-
             while (keepAlive)
             {
                 try
--- Calculator/Calculator FXSS1/Calculator_FXSS1.Tests/Calculator_FXSS1.Tests.cs
+++ Calculator/Calculator FXSS1/Calculator_FXSS1.Tests/Calculator_FXSS1.Tests.cs
@@ -18,115 +18,140 @@ namespace Calculator_FXSS1.Tests
         [Fact]
         public void AdditionWithNonumbers()
         {
-            double test = Program.Addition(25, 25);
-            Assert.Equal(50, test);
+            double expected = 50;
+
+            double actual = Program.Addition(25, 25);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void AdditionOverloadTest()
+        {
+            double expected = 152;
+
+            double actual = Program.Addition(new double[] { 25.50, 25.50, 50.50, 50.50 });
+
+            Assert.Equal(expected, actual);
         }
         [Fact]
         public void AdditionWithSimpelNegativNumbers()
         {
-            // Arrange
+           
             double expected = -1;
-            //Act
+           
             double actual = Program.Addition(-3, 2);
-            //Assert
+            
             Assert.Equal(expected, actual);
         }
         [Fact]
         public void AdditionWithBigNumbers()
         {
-            // Arrange
+            
             double expected = 6100000;
-            //Act
+            
             double actual = Program.Addition(100000, 6000000);
-            //Assert
+            
             Assert.Equal(expected, actual);
         }
         [Fact]
         public void AdditionWithDoubleNumbers()
         {
-            // Arrange
+            
             double expected = 50.30;
-            //Act
+            
             double actual = Program.Addition(20.20, 30.10);
-            //Assert
+            
             Assert.Equal(expected, actual);
         }
         [Fact]
-        public void SubbtractionWithSimpelNumbers()
+        public void SubtractionWithSimpleNumbers()
         {
-            // Arrange
+            
             double expected = 5;
-            //Act
+            
             double actual = Program.Subtraction(25, -20);
-            //Assert
+            
             Assert.NotEqual(expected, actual);
         }
         [Fact]
-        public void SubbtractionWithNegativlNumbers()
+        public void SubtractionForOverload()
         {
-            // Arrange
+            
+            double expected = 25;
+            
+            double actual = Program.Subtraction(new double[] { 25, -20, 20 });
+            
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void SubtractionWithNegativeNumbers()
+        {
+            
             double expected = -300;
-            //Act
+           
             double actual = Program.Subtraction(-100, -200);
-            //Assert
+            
             Assert.NotEqual(expected, actual);
         }
         [Fact]
-        public void SubbtractionWithDoubelNegativlNumbers()
+        public void SubtractionWithDoubleNegativeNumbers()
         {
-            // Arrange
+            
             double expected = -30.70;
-            //Act
+            
             double actual = Program.Subtraction(-10.20, -20.50);
-            //Assert
+           
             Assert.NotEqual(expected, actual);
         }
         [Fact]
-        public void MultiplicationWithSimpelNumbers()
+        public void MultiplicationWithSimpleNumbers()
         {
-            double test = Program.Multiplication(5, 5);
-            Assert.Equal(25, test);
+            double expected = 25;
+
+            double actual = Program.Multiplication(5, 5);
+
+            Assert.Equal(expected, actual);
         }
         [Fact]
-        public void DivisionWithSimpelNumbers()
+        public void DivisionWithSimpleNumbers()
         {
-            double test = Program.Division(100, 5);
-            Assert.Equal(20, test);
+            double expected = 20;
+
+            double actual = Program.Division(100, 5);
+
+            Assert.Equal(expected, actual);
         }
         [Fact]
         public void DivisionWithZeroNumbers()
         {
-            double test = Program.Division(50, 0);
-            Assert.Equal(0, test);
+            double expected = double.PositiveInfinity;
+
+            double actual = Program.Division(50, 0);
+
+            Assert.Equal(expected, actual);
         }
         [Fact]
         public void AdditionArray()
         {
+            double expected = 31;
+
             double[] number = new double[5] { 1, 5, 6, 9, 10 };
-            double test = Program.Addition(number);
-            Assert.Equal(31, test);
+
+            double actual = Program.Addition(number);
+
+            Assert.Equal(expected, actual);
         }
         [Fact]
-        public void AdditionWithnegativNumbersArray()
+        public void AdditionWithNegativeNumbersArray()
         {
+            double expected = 19;
+
             double[] number = new double[5] { 1, 5, -6, 9, 10 };
-            double test = Program.Addition(number);
-            Assert.Equal(19, test);
+
+            double actual = Program.Addition(number);
+
+            Assert.Equal(expected, actual);
         }
-        //[Fact]
-       /* public void SubbtractionArray()
-        {
-            double[] number = new double[3] { -1, -5, -6 };
-            double test = Program.Subtraction(number);
-            Assert.Equal(-12, test);
-        }
-        [Fact]
-        public void SubbtractionWithPositivNumbersArray()
-        {
-            double[] number = new double[5] { -1, -5, -6, 10, 20 };
-            double test = Program.Subtraction(number);
-            Assert.Equal(18, test);
-        }*/
     }
 }
 
